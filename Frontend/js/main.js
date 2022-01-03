15bc2da29951ce0a76a946e3a00f38a272bd0a70
reemdadreem <kareemwatts.kw@gmail.com>
2022-01-03 00:26:03 -0500

    Deployed

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -4,18 +4,16 @@
 // });
 
 const functionApiUrl = 'https://getresicounter.azurewebsites.net/api/GetResumeCounter?code=cre70RVuFmsEz7h66Ea2tpgELPj0ucJUXNZ7dFXRQ9j3i6JfFz2nyQ=='
-const localfunctionApi = 'https://visitorcounter.azurewebsites.net/api/GetCounter?code=4mYCr1McGHA4t0ls8MjUCvJ8svXjJvZoaiOuwzJzTssIsHIA1jUbvw=='; 
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter'; 
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl)
-    .then(response => {
-        return response.text()
-    })
-    .then(response => {
-        console.log("Website called function API.",response);
-        count = response;
-        document.getElementById('counter').innerText = count;
+    fetch(functionApiUrl).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
     }).catch(function(error) {
         console.log(error);
       });
